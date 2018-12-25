1fd391962f65d1a16bb7235816027e4e2faa9af7
Joshua Marx <marx.joshua@gmail.com>
2018-12-25 14:33:33 -0800

    implemented working conversion
    added C/F conversion equation
    error checking for non-valid entries

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,59 @@
 use std::io;
 
 fn main() {
-    // establish string variable 
-        // "F" for C to F
-        // "C" for F to C
-
-    // check for valid input: 1 or 2
-        // 1 sets string variable to "F"
-        // 2 sets string variable to "C"
-
-    // create function that takes in i32
-        // if string variable is "F"
-            // convert i32 to F
-        // if string variable is "C"
-            // convert i32 to "C"
-    
-
     loop {
         println!("Press 1 for C to F");
         println!("Press 2 for F to C");
+
+        let mut convert_to = String::new();
+
+        io::stdin().read_line(&mut convert_to)
+            .expect("Failed to read line");
+
+        let convert_to: u32 = match convert_to.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("{:?} is not a number", convert_to.trim());
+                continue;
+            },
+        };
+        
+        match convert_to {
+            1 => println!("Please enter temperature (in C) to convert to F"),
+            2 => println!("Please enter temperature (in F) to convert to C"),
+            _ => {
+                println!("'{}' is not a valid choice", convert_to);
+                continue;
+            },
+        };
 
         let mut temp = String::new();
 
         io::stdin().read_line(&mut temp)
             .expect("Failed to read line");
-
-        let mut temp: u32 = match temp.trim().parse() {
+        
+        let temp: i32 = match temp.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please enter a number");
+                println!("{:?} is not a valid temperature", temp.trim());
                 continue;
             },
         };
 
-        temp = check(temp);
-        
-        println!("Converted temp: {}", temp);
+        check_and_convert(convert_to, &temp);
     }
 }
 
-fn convert(temp: u32, convert_to: u32) -> u32 {
-    if convert_to == 0 {
-        println!("{}", temp);
-        1
+fn check_and_convert(convert_to: u32, temp: &i32) {
+    match convert_to {
+        1 => {
+            let new_temp = ((9 / 5) * temp) + 32;
+            println!("{}C is {}F", temp, new_temp);
+        },
+        2 => {
+            let new_temp = ((temp - 32) * 5) / 9;
+            println!("{}F is {}C", temp, new_temp);
+        },
+        _ => (),
     }
-
-    if convert_to == 1 {
-        println!("{}", temp);
-        2
-    }
-}
-
-fn check(temp: u32) -> u32 {
-    if temp == 1 {
-            println!("Converting F to C");
-            convert(temp, 0)
-        } else if temp == 2 {
-            println!("Converting C to F");
-            convert(temp, 1)
-        } else {
-            println!("Please enter valid option");
-            0
-        }
 }
